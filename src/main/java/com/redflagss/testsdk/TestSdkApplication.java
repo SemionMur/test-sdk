8e996337bba616f26202dcc2a8ce950eadbe0062
SemionMur <simeon.muravyov@gmail.com>
2026-03-04 20:34:20 +0300

    init commit

---
+++ src/main/java/com/redflagss/testsdk/TestSdkApplication.java
@@ -0,0 +1,13 @@
+package com.redflagss.testsdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSdkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSdkApplication.class, args);
+    }
+
+}
